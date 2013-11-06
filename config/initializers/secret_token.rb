bc92731999e5179c264119dabbd5221472894094
BrwnSuperman <huq.tanveer@gmail.com>
2013-11-06 15:09:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PluribusFund::Application.config.secret_key_base = '6e6c78367ad572029e49c45d883dfe834b29b083ca4d5f44967243ce588691b8d14d370c8e7caef73598e784d07d2b229c5d443c1bbaa986cc5a9b8800bdb8cd'
